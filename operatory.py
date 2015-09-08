f09c4fc903c7d1e51728738939ea5c5bfb19ec9a
Łukasz Ćwikliński <artofbw@gmail.com>
2015-09-09 00:19:31 +0200

    Wstęp 1
    operatory

---
+++ operatory.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# sprawdzenie typu:
+type(wartosc)
+
+Operatory:
+
+Arytmetyczne
+
++ dodawanie (można dodawać łanuchy znaków)
+- odejmowanie
+* mnożenie
+/ dzielenie (3/2 = 1.5)
+// dzielenie całkowite (3/2 = 1)
+% modulo (reszta z dzielenia)
+divmod(3,2) czyli 3/2 zwraca wynik dzielenia całkowitego oraz wynik modulo (1, 1)
+** potęgowanie (także jako pierwiastek) 4**4 lub 8**(1/3)
+
+
+Logiczne
+
+> większy niż
+< mniejszy niż
+>= nie mniejszy (większy lub równy)
+<= nie większy (mniejszy lub równy)
+!= rożny
+== równy
+and
+or
+not
